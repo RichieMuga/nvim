2f12ad63c8a64102cbdac5f86b52c39d5fe9716e
Richie Mugambi <richiemugambi4@gmail.com>
2024-05-01 00:00:20 +0300

    deps: added nvim core deps

---
+++ lua/richie/packer.lua
@@ -0,0 +1,59 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
+
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+
+    use('nvim-treesitter/playground')
+
+    use('ThePrimeagen/harpoon')
+
+    use('mbbill/undotree')
+
+    use('tpope/vim-fugitive')
+
+    use { "catppuccin/nvim", as = "catppuccin" }
+
+    use { 'wakatime/vim-wakatime' }
+
+    use {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    }
+
+    -- autocompletion
+    use("hrsh7th/nvim-cmp")   -- completion plugin
+    use("hrsh7th/cmp-buffer") -- source for text in buffer
+    use("hrsh7th/cmp-path")   -- source for file system paths
+
+    -- snippets
+    use("L3MON4D3/LuaSnip")             -- snippet engine
+    use("saadparwaiz1/cmp_luasnip")     -- for autocompletion
+    use("rafamadriz/friendly-snippets") -- useful snippets
+
+    use { 'nvim-telescope/telescope-ui-select.nvim' }
+
+    use({
+        "nvimtools/none-ls.nvim",
+        config = function()
+            require("null-ls").setup()
+        end,
+        requires = { "nvim-lua/plenary.nvim" },
+    })
+    use({"lewis6991/gitsigns.nvim"})
+
+end
+
+)
