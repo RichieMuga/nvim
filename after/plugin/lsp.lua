64ea86f539f199a25e59cc3655d8f102b1352421
Richie Mugambi <richiemugambi4@gmail.com>
2024-05-02 10:46:26 +0300

    refactor: clean up lua files to be in table format

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,51 +1,72 @@
-require("mason").setup()
-require("mason-lspconfig").setup {
-    auto_install = true
-}
+-- Import packer.nvim
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
 
--- Keybindings configarations
+  -- LSP and Completion
+  use {
+    'williamboman/mason.nvim',
+    config = function()
+      require('mason').setup()
+    end
+  }
 
-local lspconfig = require("lspconfig")
+  use {
+    'williamboman/mason-lspconfig.nvim',
+    config = function()
+      require('mason-lspconfig').setup()
+    end
+  }
 
--- Bindings for lua
-lspconfig.lua_ls.setup({})
-lspconfig.tsserver.setup({})
+  use {
+    'neovim/nvim-lspconfig',
+    config = function()
+      local nvim_lsp = require('lspconfig')
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+      -- Configure TypeScript Language Server
+      nvim_lsp.tsserver.setup {
+        capabilities = capabilities,
+        on_attach = function(client, bufnr)
+          -- Enable completion triggered by <c-x><c-o>
+          vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-local on_attach = function(client, bufnr)
-  -- Enable 'tagfunc' for "go to definition" capabilities
-  client.server_capabilities.definitionProvider = true
-  vim.lsp.handlers["textDocument/definition"] = vim.lsp.with(
-    vim.lsp.handlers.definition, {
-      tagfunc = "lsp#tagfunc"
+          -- Use the client in the on_attach function
+          client.server_capabilities.definitionProvider = true
+          client.server_capabilities.documentFormattingProvider = true
+        end
+      }
+
+      -- Set up nvim-cmp
+      local cmp = require('cmp')
+      cmp.setup {
+        sources = {
+          { name = 'nvim_lsp' },
+          { name = 'buffer' },
+        },
+        mapping = {
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.close(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        },
+      }
+
+      -- Set global keymaps for LSP
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = true })
+      vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = true })
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = true })
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { buffer = true })
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { buffer = true })
+    end
+  }
+
+  -- Autocompletion and snippets
+  use {
+    'hrsh7th/nvim-cmp',
+    requires = {
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+      'rafamadriz/friendly-snippets'
     }
-  )
-
-  -- Enable 'omnifunc' for manual omni mode completion
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Enable 'formatexpr' for LSP formatting with 'gq'
-  client.server_capabilities.documentFormattingProvider = true
-  vim.lsp.handlers["textDocument/formatting"] = vim.lsp.with(
-    vim.lsp.handlers.formatting, {
-      formatexpr = "lsp#formatexpr()"
-    }
-  )
-
-  -- Keymaps
-  local bufopts = { noremap = true, silent = true, buffer = bufnr }
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('i', '<C-S>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, bufopts)
-  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, bufopts)
-  vim.keymap.set('n', '<leader>dl', vim.diagnostic.open_float, bufopts)
-end
-
--- Setup LSP servers with on_attach callback
-require('lspconfig').pyright.setup { on_attach = on_attach }
-require('lspconfig').tsserver.setup { on_attach = on_attach }
--- TODO: Add more server setups here
+  }
+end)
