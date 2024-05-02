7a596f3d762c810f1fab78f911ccb338bed9c05f
Richie Mugambi <richiemugambi4@gmail.com>
2024-05-02 10:46:26 +0300

    refactor: clean up lua files to be in table format

--- after/plugin/none-ls.lua
+++ after/plugin/none-ls.lua
@@ -1,15 +1,22 @@
-local null_ls = require("null-ls")
-
-null_ls.setup({
-    sources = {
+return {
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
+        null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.isort,
-        null_ls.builtins.formatting.gofumpt,
+        null_ls.builtins.diagnostics.flake8,
         null_ls.builtins.diagnostics.golangci_lint,
-        null_ls.builtins.diagnostics.markdownlint
-    },
-})
+        null_ls.builtins.diagnostics.markdownlint,
+        null_ls.builtins.formatting.gofumpt,
+        null_ls.builtins.formatting.markdownlint,
+        null_ls.builtins.formatting.isort,
+      },
+    })
 
-vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
+}
