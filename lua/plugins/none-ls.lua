8b965f7149f888faa9de1e1757ae98aa19ea9e86
Richie Mugambi <richiemugambi4@gmail.com>
2024-06-20 11:44:13 +0300

    feat: add disable highlighting for dockerfile

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -14,7 +14,7 @@ return {
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
         null_ls.builtins.formatting.sql_formatter,
-        null_ls.builtins.diagnostics.sqlfluff
+        null_ls.builtins.diagnostics.sqlfluff,
       },
     })
 
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,8 +5,9 @@ return {
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
+        ensure_installed = { "dockerfile" },
         auto_install = true,
-        highlight = { enable = true },
+        highlight = { enable = true, disable = { "dockerfile" }},
         indent = { enable = true },
       })
     end
