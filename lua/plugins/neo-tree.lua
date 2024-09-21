315c40ef13dd081c5482c01103164d5fe412e2ba
Richie Mugambi <richiemugambi4@gmail.com>
2024-09-21 11:58:19 +0300

    feat(chore): fixed neotree and removed depreciated tsserver, instead added ts_ls

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -3,12 +3,22 @@ return {
   branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
+    "nvim-tree/nvim-web-devicons", -- optional, but recommended for icons
     "MunifTanjim/nui.nvim",
+    -- "3rd/image.nvim", -- Optional: uncomment if you want image support in the preview window
   },
   config = function()
-    vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {})
-    vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
-    vim.keymap.set("n", "<C-l>", ":Neotree filesystem reveal left<CR>", {})
+    -- Key mappings for neo-tree
+    vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", { silent = true, noremap = true })
+    vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", { silent = true, noremap = true })
+    vim.keymap.set("n", "<C-l>", ":Neotree filesystem reveal left<CR>", { silent = true, noremap = true })
+
+    -- Additional options for neo-tree if required
+    require("neo-tree").setup({
+      filesystem = {
+        follow_current_file = true, -- This option follows the current file in the tree
+        hijack_netrw_behavior = "open_default", -- Replaces netrw with neo-tree
+      },
+    })
   end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,23 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
+    build = ":TSUpdate",  -- Ensures parsers are updated automatically
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
-        ensure_installed = { "dockerfile" },
+        -- Install parsers you need; add more languages if required
+        ensure_installed = { "lua", "dockerfile" },  -- Added "lua" to ensure the Lua parser is installed
+        -- Automatically install missing parsers when entering buffer
         auto_install = true,
-        highlight = { enable = true, disable = { "dockerfile" }},
+        -- Enable Treesitter-based syntax highlighting
+        highlight = {
+          enable = true,  -- Enable for all supported languages
+          disable = {},   -- You can list languages to disable here if needed
+        },
+        -- Enable indentation support where applicable
         indent = { enable = true },
       })
     end
   }
 }
+
